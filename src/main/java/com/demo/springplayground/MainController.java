193ab287270fe0a318760af1908316bf6984150f
kai_chin <kaibankpro@gmail.com>
2026-06-24 16:36:49 +0800

    Greeting

--- src/main/java/com/demo/springplayground/MainController.java
+++ src/main/java/com/demo/springplayground/MainController.java
@@ -1,9 +1,16 @@
 package com.demo.springplayground;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
-    
+    @GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 }
--- src/main/java/com/demo/springplayground/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.demo.springplayground;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class StudentService {
-    
-}
